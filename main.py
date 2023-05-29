16e7d396d0319f048b7cdd108ccb3fc3d821c697
Giovanni <barbierig25@yahoo.com.br>
2023-05-29 14:47:54 -0300

    more development

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.scrollview import MDScrollView
 import kivy.core.window
+from kivy.properties import StringProperty, NumericProperty, ListProperty
 import ssl
 from kivy.utils import platform
-if platform=="android":
-        from android.permissions import request_permissions, Permission
-        request_permissions([Permission.CAMERA])
+if platform == "android":
+    from android.permissions import request_permissions, Permission
+    request_permissions([Permission.CAMERA])
 
 
 # Para a internet funfar no app
 ssl._create_default_https_context = ssl._create_unverified_context
+
 
 class CartaTab(MDScrollView):
     pass
@@ -21,8 +23,41 @@ class CartaTab(MDScrollView):
 class ScannerTab(MDFloatLayout):
     pass
 
+class ListaTab(MDScrollView):
+    pass
 class HomePage(MDFloatLayout):
-    pass    
+    pass
+
+class CartaNaLista(MDFloatLayout):
+    image = StringProperty()
+    name = StringProperty()
+    set_name = StringProperty()
+    acabamento = StringProperty()
+    qnt = NumericProperty()
+    preco = NumericProperty()
+
+class LinhaPreco(MDGridLayout):
+    acabamento = StringProperty()
+    preco = NumericProperty()
+
+
+class ListaPrecos(MDBoxLayout):
+    set_name = StringProperty()
+    pass
+
+
+class SetCard(MDBoxLayout):
+
+    set_name = StringProperty()
+    lista_precos = ListProperty()
+
+    # def __init__(self, **kwargs):
+    #     super(SetCard, self).__init__(**kwargs)
+    #     print(self.ids)
+    #     lista = getattr(self.ids, "lista")
+    #     lista.add_widget(MDLabel(text="aaaaaaaaaaaaaaaaaaaa"))
+    #     self.add_widget(lista)
+        
 
 class MyApp(MDApp):
     def build(self):
@@ -32,5 +67,6 @@ class MyApp(MDApp):
         self.theme_cls.theme_style = "Dark"
         return HomePage()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     MyApp().run()
